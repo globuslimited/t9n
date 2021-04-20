481ffbf8360888bae18427d420e8f6b5ab652daa
Andrei izotov <faradaytrs@gmail.com>
2021-04-20 14:46:44 +0800

    export TranslationProvider

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+import {TranslationContext} from "./context";
 export {TranslationContext} from "./context";
 export {useTranslation} from "./useTranslation";
+export const TranslationProvider = TranslationContext.Provider;
