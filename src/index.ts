d3b08b273db64a64153210eee4d4c5d52d6bbcde
Andrei izotov <faradaytrs@gmail.com>
2021-09-06 11:33:26 +0800

    export template function

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export { TranslationContext } from "./context";
 export { useTranslation } from "./useTranslation";
 export const TranslationProvider = TranslationContext.Provider;
 export { TranslationProperties, UseTranslationResponse } from "./useTranslation";
-export { TranslationMap, Translation, TranslationSettings, Language } from "./context";
+export { TranslationMap, Translation, TranslationSettings, TemplateFunction, Language } from "./context";
 export { translation } from "./translation";
