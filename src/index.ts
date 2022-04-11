7ddc2e301639e83db75f4103effd0f43de4fa249
haonan <214611026@qq.com>
2022-04-11 13:30:32 +0800

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import {TranslationContext} from "./context.js";
+export type {PackedPlugin} from "./plugin.js";
+export type {UseTranslationResponse, DictFunction} from "./useTranslation.js";
+export type {TranslationSettings} from "./context.js";
+export type {TranslationMap, Translation, TemplateFunction, TranslationProperties} from "./basic.js";
+
 export {TranslationContext} from "./context.js";
 export {useTranslation} from "./useTranslation.js";
-export const TranslationProvider = TranslationContext.Provider;
-export {UseTranslationResponse, DictFunction} from "./useTranslation.js";
-export {TranslationSettings} from "./context.js";
-export {TranslationMap, Translation, TemplateFunction, Language, TranslationProperties} from "./basic.js";
-export {plugin, PackedPlugin} from "./plugin.js";
+export {Language} from "./basic.js";
+export {plugin} from "./plugin.js";
 export {translation} from "./translation.js";
+
+import {TranslationContext} from "./context.js";
+export const TranslationProvider = TranslationContext.Provider;
