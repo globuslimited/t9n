1a9d4153fdcdac98a6609169119bd590754df4f0
haonan <214611026@qq.com>
2022-11-29 19:16:05 +0800

    export generateDictFunction

--- src/shared.ts
+++ src/shared.ts
@@ -1,9 +1,10 @@
 export type {PackedPlugin} from "./plugin.js";
 export type {UseTranslationResponse} from "./useTranslation.js";
-export type {DictFunction} from "./translationFunction.js"
+export type {DictFunction} from "./translationFunction.js";
 export type {TranslationSettings} from "./settings.js";
 export type {TranslationMap, Translation, TemplateFunction, TranslationProperties} from "./basic.js";
 
 export {Language} from "./basic.js";
 export {plugin} from "./plugin.js";
 export {translation} from "./translation.js";
+export {generateDictFunction} from "./translationFunction.js";
