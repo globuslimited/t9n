44935c7e44fbb7c9abb18fb4d7e14637d719b47a
Andrei izotov <faradaytrs@gmail.com>
2023-01-17 11:59:22 +0800

    draft template extension

--- src/extensions/index.ts
+++ src/extensions/index.ts
@@ -4,3 +4,10 @@ import {russianPlurals} from "./russian/plurals.js";
 export const russian = {
     plurals: russianPlurals,
 } satisfies Record<string, Extension<any, any>>;
+
+russian.plurals<{additionalArgument: number}>({
+    0: "1",
+    1: "2",
+    2: "3"
+})
+
---
+++ src/extensions/template.ts
@@ -0,0 +1,12 @@
+import { createExtension } from "./extension.js";
+
+export const template = createExtension<string, {}>(
+    (translation, options, language) => {
+        let str = translation;
+        Object.entries(options).forEach(([name, value]) => {
+            str = str.replaceAll(`{{${name}}}`, value.toString())
+        });
+        return str;
+    },
+    ["ru", "en"],
+);
