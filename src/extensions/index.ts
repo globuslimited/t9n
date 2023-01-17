22af2c23a43d08499f8d19653398181fc45abebb
Andrei izotov <faradaytrs@gmail.com>
2023-01-17 14:39:39 +0800

    add some test code

--- src/extensions/index.ts
+++ src/extensions/index.ts
@@ -3,7 +3,7 @@ import {Extension} from "./extension.js";
 import {russianPlurals} from "./russian/plurals.js";
 export {sex} from "./others/sex.js";
 export {template} from "./others/template.js";
-export {createExtension} from "./extension.js"
+export {createExtension} from "./extension.js";
 
 export const russian = {
     plurals: russianPlurals,
@@ -13,14 +13,27 @@ export const english = {
     plurals: englishPlurals,
 } satisfies Record<string, Extension<any, any>>;
 
-const test = russian.plurals<{additionalArgument: number}>({
-    0: "1",
-    1: "2",
-    2: "3",
-});
-type Test = typeof test extends {__isExtension: true} ? true: false
+const translation = {
+    en: {
+        hello: english.plurals<{additionalArgument: number}>({
+            singular: "hello",
+            plurals: "hello everyone",
+        }),
+    },
+    ru: {
+        hello: russian.plurals<{additionalArgument: number}>({
+            0: "1",
+            1: "2",
+            2: "3",
+        }),
+    },
+};
 
-english.plurals<{additionalArgument: number}>({
-    singular: "hello",
-    plurals: "hello everyone",
-});
+type Test1 = typeof translation[keyof typeof translation];
+type Extension1 = Test1["hello"];
+type Handler1 = Extension1["handler"];
+type HandlerProperties = Parameters<Handler1>[0]
+const testProperties = {
+    count: 3,
+    additionalArgument: 3
+} satisfies HandlerProperties
