bd5c876d1a76fac81932836b09841670dcc13e0d
Andrei izotov <faradaytrs@gmail.com>
2023-01-17 12:49:13 +0800

    rename CustomOptions to TemplateOptions

--- src/extensions/extension.ts
+++ src/extensions/extension.ts
@@ -1,22 +1,22 @@
 type Handler<Options> = (options: Options, language: string) => string;
 
-export type Extension<Arguments, BasicOptions extends {}> = <CustomOptions extends {}>(
+export type Extension<Arguments, BasicOptions extends {}> = <TemplateOptions extends {}>(
     translation: Arguments,
 ) => {
     __isExtension: true;
     supportedLanguages?: string[];
-    handler: Handler<BasicOptions & CustomOptions>;
+    handler: Handler<BasicOptions & TemplateOptions>;
 };
 
 export const createExtension = <Arguments, BasicOptions extends {} = {}>(
     translate: <Options>(translation: Arguments, options: Options, language: string) => string,
     supportedLanguages?: string[],
 ): Extension<Arguments, BasicOptions> => {
-    return <CustomOptions extends {} = {}>(translation: Arguments) => {
+    return <TemplateOptions extends {} = {}>(translation: Arguments) => {
         return {
             __isExtension: true,
             supportedLanguages,
-            handler: (options: BasicOptions & CustomOptions, language) => translate(translation, options, language),
+            handler: (options: BasicOptions & TemplateOptions, language) => translate(translation, options, language),
         };
     };
 };
